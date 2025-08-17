dc021a64eae57dc41c48ce20cfc678c1705b284a
adelard <adelarddcunha@gmail.com>
2025-08-17 22:41:33 +0530

    added pdf text extraction function

--- src/data_ingestion/data_extraction.py
+++ src/data_ingestion/data_extraction.py
@@ -1,29 +1,53 @@
 import logging
-from src.utils import logging
 import os
+from PyPDF2 import PdfReader
 
+from src.utils import logging
 
 class DataExtractionInitialise:
 	def __init__(self):
 		self.datapath = 'objects/data'
 
-	def read_files(self):
-		self.filenames = os.listdir(self.datapath)
-		return self.filenames
+	def read_file_names(self):
+		try:
+			self.filenames = os.listdir(self.datapath)
+			return self.filenames
+		except Exception as e:
+			logging.error(f"Error occured during reading filepath. {e}")
 
 
 class DataExtraction:
-	def __init__(self):
-		self.data_extraction_init = DataExtractionInitialise()
-		self.filenames = self.data_extraction_init.read_files()
+	def extract_data(self, filepath):
+		try:
+			text = ""
+			reader = PdfReader(filepath)
+			for page_num in range(len(reader.pages)):
+				if page_num <=10:
+					page = reader.pages[page_num]
+					text += " " + page.extract_text()
+					logging.info(f"Extracted page {page_num}")
+			return text
 
+		except Exception as e:
+			logging.error(f"Error occured during reading file. {e}")
 
-	def extract_data(self):
-		for filename in self.filenames:
-			logging.info(filename)
+def main():
+	data_extraction_init = DataExtractionInitialise()
+	filenames = data_extraction_init.read_file_names()
+
+	data_extraction = DataExtraction()
+	data = []
+	for filename in filenames[:1]: # currently reading only the first file
+		filepath = os.path.join(data_extraction_init.datapath, filename)
+		logging.info(filepath)
+
+		text = data_extraction.extract_data(filepath)
+		data.append(text)
+	return data
+
 
 
 
 if __name__=="__main__":
-	data_extraction = DataExtraction()
-	data_extraction.extract_data()
+	data = main()
+	print(data)
