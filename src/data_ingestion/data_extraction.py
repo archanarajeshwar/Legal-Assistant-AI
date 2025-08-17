120916e2dad9398326ea7408249ad58d9645e509
adelard <adelarddcunha@gmail.com>
2025-08-17 22:10:14 +0530

    added basic folde structure

---
+++ src/data_ingestion/data_extraction.py
@@ -0,0 +1,29 @@
+import logging
+from src.utils import logging
+import os
+
+
+class DataExtractionInitialise:
+	def __init__(self):
+		self.datapath = 'objects/data'
+
+	def read_files(self):
+		self.filenames = os.listdir(self.datapath)
+		return self.filenames
+
+
+class DataExtraction:
+	def __init__(self):
+		self.data_extraction_init = DataExtractionInitialise()
+		self.filenames = self.data_extraction_init.read_files()
+
+
+	def extract_data(self):
+		for filename in self.filenames:
+			logging.info(filename)
+
+
+
+if __name__=="__main__":
+	data_extraction = DataExtraction()
+	data_extraction.extract_data()
