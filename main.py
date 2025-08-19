f3a4fa855fa04a58b64733eb7ca87783a86fd449
adelard <adelarddcunha@gmail.com>
2025-08-19 08:15:13 +0530

    modifications: requirements.txt, utils.logger.py, main.py, src.vector_db.create_vector.py, .env

---
+++ main.py
@@ -0,0 +1,16 @@
+from src.vector_db import LegalVectorDB
+
+
+db = LegalVectorDB()
+
+# Add documents (only once)
+#db.add_documents(legal_data)
+
+# Run a query
+query = "What is a cognizable offence?"
+results = db.query(query)
+
+print("Search Results:\n")
+for doc, meta in zip(results["documents"][0], results["metadatas"][0]):
+    print(f"Section {meta['section']} | {meta['title']}")
+    print(doc[:250], "...\n")
